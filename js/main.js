c6bbd126c6115897a18d7e300779c7269f32512c
Vinicius Lopes Alves <vilopesalves@hotmail.com>
2020-05-23 04:07:39 -0300

    jquery

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function($){
+
+    var $doc = $('html, body');
+    $('.scroll-page').click(function(){
+        $doc.animate({
+            scrollTop: $($.attr(this,'href')).offset().top
+        },750);
+        return false;
+    })
+
+})(jQuery);
